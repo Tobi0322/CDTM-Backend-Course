caefdf514b97c4699928e89c9d8c723dd5ac3061
Michael Fröhlich <M.Froehlich@tum.de>
2016-09-12 19:53:14 +0200

    Add Flask hello world server

--- demo/step01/step01.py
+++ demo/step01/step01.py
@@ -1,12 +1,22 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
-
+# say hello
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+# will trigger an error
+@app.route('/err')
+def err():
+    assert app.debug == False
+    return 'Oops!'
+
 
 if __name__ == '__main__':
-    app.run()
+    addr = "localhost"         # the same as 127.0.0.1
+    port = int("20000")        # between 3000 and 65535
+    debug = True               # activates the [1] debugger, [2] automatic reloader
+    app.run(host=addr, port=port, debug=debug)
