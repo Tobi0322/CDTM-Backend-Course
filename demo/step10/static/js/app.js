37c9d45f6cbe4d8361d700690e4b66fe95f508d6
Michael Fröhlich <M.Froehlich1994@gmail.com>
2016-11-04 00:27:34 +0100

    downgrade to jQuery 2.2.4 to fix landing page parallax effect

--- demo/step10/static/js/app.js
+++ demo/step10/static/js/app.js
@@ -24,16 +24,20 @@ function clearSelection() {
 }
 
 function initMaterializeComponents() {
-  $('.button-collapse').sideNav();
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal-trigger').leanModal();
+  try {
+    $('.button-collapse').sideNav();
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
 
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
 
-  $('.parallax').parallax();
+    $('.parallax').parallax();
+  } catch(err) {
+    // ignore
+  }
 }
 
 app.config(function($locationProvider, $routeProvider) {
