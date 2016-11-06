95d1c66433a44f1e6a05a3f0e12d44af664451a1
Michael Fröhlich <M.Froehlich1994@gmail.com>
2016-11-06 20:50:02 +0100

    remove comment

--- demo/step12/server/routes/auth.py
+++ demo/step12/server/routes/auth.py
@@ -11,7 +11,6 @@ def register():
     data = request.get_json(force=True)
     email = data.get('email', '').lower()
     password = data.get('password')
-    # TODO: properly check for email
     if email == None or (not isEmail(email)) or password == None or len(password) < 6:
         return jsonify({'result': False, 'text': 'Invalid username and/or password'})
     if db_create_user(email, security.generate_password_hash(password)):
