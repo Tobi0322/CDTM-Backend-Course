52c239e7d19238d9ce8cad82c8d077b00f0904a6
FroeMic <m.froehlich@tum.de>
2016-11-23 21:07:25 +0100

    Reload Tasks automatically each 10 Seconds

--- demo/step13/server/static/app/controllers/mainCtrl.js
+++ demo/step13/server/static/app/controllers/mainCtrl.js
@@ -1,13 +1,14 @@
 app.controller('mainCtrl', function($scope, $interval, $timeout, AuthService, TaskService) {
 
   // TODO: find a better solution to busy waiting
-  // FIXME: overwrites current tasks. If the user changes single tasks this leads to reference errors
-  // var loadTasks = $interval(function() {
-  //   if (AuthService.isLoggedIn()) {
-  //     TaskService.loadTasks(false);
-  //   } else {
-  //     $interval.cancel(loadTasks);
-  //   }
-  // }, 10000);
+  var reloadTasks = $interval(function() {
+    if (AuthService.isLoggedIn()) {
+      TaskService.lists.forEach(function(list) {
+        TaskService.loadTasks(false, list.id);
+      })
+    } else {
+      $interval.cancel(reloadTasks);
+    }
+  }, 10000);
 
 });
