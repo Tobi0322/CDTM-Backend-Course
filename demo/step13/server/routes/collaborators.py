37fbc06305df3253fcb89a3ae282c5a8b0d2007a
FroeMic <m.froehlich@tum.de>
2016-11-24 11:18:09 +0100

    updated CREATE collaborator route

--- demo/step13/server/routes/collaborators.py
+++ demo/step13/server/routes/collaborators.py
@@ -5,21 +5,27 @@ from server import app
 from server.database import *
 from server.utils import login_required, list_owner, json_abort
 
-@app.route('/api/lists/<string:list_id>/collaborators/<string:collaborator_id>', methods=['POST'])
+@app.route('/api/lists/<string:list_id>/collaborators/<string:collaborator_email>', methods=['POST'])
 @login_required
 @list_owner
-def add_collaborator(list_id, task_id, collaborator_id):
-    if db_get_user_by_id(collaborator_id) == None:
-        json_abort(400, 'Invalid request parameters')
+def add_collaborator(list_id, task_id, collaborator_email):
+    user = db_get_user(collaborator_email) == None:
+    if user == None:
+        json_abort(404, 'User not found')
 
     l = db_get_list(list_id)
     if l == None:
-        json_abort(500, 'Could not update list')
+        json_abort(404, 'List not found')
+
+    db_add_collaborator(l.id, user.id)
+    l = db_get_list(list_id)
+    if l == None:
+        json_abort(404, "List not found")
 
     return jsonify(l.__dict__)
 
 
-# REMOVE FILE
+# REMOVE collaborator
 @app.route('/api/lists/<string:list_id>/collaborators/<string:collaborator_id>', methods=['DELETE'])
 @login_required
 @list_access
