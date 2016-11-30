d4897a3722ee2f1fc428d2bc8c9171f3c69a655b
FroeMic <m.froehlich@tum.de>
2016-11-30 22:32:59 +0100

    update config

--- demo/step13/config.py
+++ demo/step13/config.py
@@ -4,11 +4,11 @@ VERSION = 13.0
 HOST    = 'localhost'
 PORT    = 20013
 
-SECRET = 'mostly awesome1'
+SECRET = 'mostly awesome'
 
 # This is the path to the upload directory
 UPLOAD_FOLDER = 'uploads/'
 # These are the extension that we are accepting to be uploaded
-ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
+ALLOWED_EXTENSIONS = ['txt', 'md', 'pdf', 'doc', 'docx', 'xls', 'xlsx', 'png', 'jpg', 'jpeg', 'gif']
 
-DB_SEED = True
+DB_SEED = False
